19d84cdf4519524f5fc14be210888c4cb14c82ca
Mustafa Ünlü <mustafa.unlu74001@gmail.com>
2022-11-15 12:24:19 +0000

    Add getCustomer method to DataSource class

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -2,6 +2,8 @@ package bank;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DataSource {
@@ -23,9 +25,41 @@ public class DataSource {
     return connection;
   }
 
+  public static Customer getCustomer(String userName) {
+
+    String sql = "select * from customers where username= ?";
+
+    Customer customer = null;
+
+    try (Connection connection = connect();
+        PreparedStatement statement = connection.prepareStatement(sql)) {
+
+      statement.setString(1, userName);
+
+      try (ResultSet resultset = statement.executeQuery()) {
+
+        customer = new Customer(
+            resultset.getInt("id"),
+            resultset.getString("name"),
+            resultset.getString("username"),
+            resultset.getString("password"),
+            resultset.getInt("account_id"));
+
+      }
+
+    } catch (SQLException ex) {
+      ex.printStackTrace();
+    }
+
+    return customer;
+  }
+
   public static void main(String[] args) {
 
-    connect();
+    // connect();
+    Customer customer = getCustomer("twest8o@friendfeed.com");
+
+    System.out.println(customer.getName());
 
   }
 
