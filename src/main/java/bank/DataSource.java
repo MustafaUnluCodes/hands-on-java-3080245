542c3ccc294ddf53d6cf0eafe027488971ed0f65
Mustafa Ünlü <mustafa.unlu74001@gmail.com>
2022-11-15 12:02:54 +0000

    Add datasource class
    add connection method.

---
+++ src/main/java/bank/DataSource.java
@@ -0,0 +1,33 @@
+package bank;
+
+import java.net.ConnectException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataSource {
+
+  public static Connection connect() {
+
+    String dbFile = "jdbc:sqlite:resources/bank.db";
+
+    Connection connection = null;
+
+    try {
+      connection = DriverManager.getConnection(dbFile);
+
+      System.out.println("db connection is established.");
+    } catch (SQLException ex) {
+      ex.printStackTrace();
+    }
+
+    return connection;
+  }
+
+  public static void main(String[] args) {
+
+    connect();
+
+  }
+
+}
